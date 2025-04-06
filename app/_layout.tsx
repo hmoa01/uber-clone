3e3552a6ed039e2886558359c59cfc3b555d7e1b
hmoa01 <hmoa01@gmail.com>
2025-04-06 19:17:47 +0200

    add onboarding page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,23 @@
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-
+import { useFonts } from 'expo-font';
 import 'react-native-reanimated';
 import '../global.css';
 
-SplashScreen.preventAutoHideAsync()
-  .then((result) =>
-    console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`)
-  )
-  .catch(console.warn);
-
 export default function RootLayout() {
-  // Set the animation options. This is optional.
-  SplashScreen.setOptions({
-    duration: 1000,
-    fade: true,
+  const [loaded] = useFonts({
+    'Jakarta-Bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'Jakarta-ExtraBold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'Jakarta-ExtraLight': require('../assets/fonts/PlusJakartaSans-ExtraLight.ttf'),
+    'Jakarta-Light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+    'Jakarta-Medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'Jakarta-Regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'Jakarta-SemiBold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
   });
-
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
