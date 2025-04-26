e025b017abfc65b48f227034abe1ca42fd02071e
hmoa01 <hmoa01@gmail.com>
2025-04-26 14:54:47 +0200

    fix nativewind issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,25 @@
-import { Stack } from 'expo-router';
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
 import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
-import '../global.css';
+import { LogBox } from 'react-native';
+
+import { tokenCache } from '@/lib/auth';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  );
+}
+
+LogBox.ignoreLogs(['Clerk:']);
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -10,15 +28,30 @@ export default function RootLayout() {
     'Jakarta-ExtraLight': require('../assets/fonts/PlusJakartaSans-ExtraLight.ttf'),
     'Jakarta-Light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
     'Jakarta-Medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
-    'Jakarta-Regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    Jakarta: require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
     'Jakarta-SemiBold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
   });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
