4db80eb7598057064bea28ce334e06841dc320ea
hmoa01 <hmoa01@gmail.com>
2025-07-01 15:52:00 +0200

    add real data and  oAuth

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -3,8 +3,10 @@ import { useRef, useState } from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Swiper from 'react-native-swiper';
-import { onboarding } from '../../constants';
+
 import CustomButton from '@/components/CustomButton';
+
+import { onboarding } from '../../constants';
 
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
